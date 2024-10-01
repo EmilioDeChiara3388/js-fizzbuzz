e96373fefc1297abb5b9950c71f509c4f1af476b
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-01 14:13:08 +0200

    add: comments to divide work

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,18 @@
 console.log("Funziona?");
+
+/* Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+Prima di partire a scrivere codice poniamoci qualche domanda: Come faccio a sapere se un numero è divisibile per? Abbiamo visto qualcosa di particolare che possiamo usare?
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+NB. Mi raccomando come visto in classe stamattina non va bene copiare la traccia per farsi fare l'esercizio ma é ottimo seguire il flusso che vi ho mostrato durante la correzione dell'esercizio.
+Scrivetevi le 5 fasi, poi createvi dello pseudo codice per ciascuna fase e procedete passo passo nella risoluzione del problema proposto.
+ */
+
+// Preparazione
+
+// Raccolta Dati
+
+// Elaborazione Codice
+
+// Output
